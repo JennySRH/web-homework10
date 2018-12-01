dceafa412495028ebb6da4c4b649605f7818bb6e
Jenny <1056627011@qq.com>
2018-12-01 23:09:16 +0800

    s2

---
+++ S2/index.js
@@ -0,0 +1,66 @@
+window.onload = function () {
+    // init
+    $("#button").on("mouseenter", function () {
+        $(".button").removeClass("disabled");
+        $("#info-bar").addClass("disabled");
+        $(".unread").hide();
+        $(".info").text("");
+    });
+
+     $(".button").click(function(event,callback) {
+        var currentId = event.currentTarget.id;
+        var currentButton = '#'+ event.currentTarget.id;
+        if(!$(currentButton).hasClass("disabled") && 
+        $(currentButton).children(".unread:hidden").length>=1){
+            if (!$(".button:not(.disabled),.button:has(.unread:hidden)").length) {
+                $("#info-bar").removeClass("disabled");
+            }
+            $(currentButton).children(".unread").text("...").show();
+            $(currentButton).siblings().addClass("disabled");
+            $.get('/'+ currentId, function (data) {
+                if (!$(currentButton).hasClass("disabled") && 
+                !$(currentButton).children(".disabled:hidden").length) {
+                    $(currentButton).children(".unread").text(data);
+                    $(currentButton).addClass("disabled");
+                    $(currentButton).siblings(":has(.unread:hidden)").removeClass("disabled");
+                }
+                if (!$(".button:not(.disabled),.button:has(.unread:hidden)").length) {
+                    $("#info-bar").removeClass("disabled");
+                }
+                if($.isFunction(callback)){
+                    callback();
+                }
+            });            
+        }
+
+    });
+
+    $("#info-bar").click(function(event){
+        if (!$("#info-bar").hasClass("disabled")) {
+            var sum = 0;
+            $(".unread").each(function () {
+                sum += parseInt($(this).text());
+            });
+            $(".info").text(sum);
+            $("info-bar").addClass("disabled");
+        }
+    });
+
+    $(".apb").click(function () {
+        $("#button").trigger("mouseenter");
+        $("#a").triggerHandler('click',function() {
+            $("#b").triggerHandler('click',function() {
+                $("#c").triggerHandler('click',function(){
+                    $("#d").triggerHandler('click',function() {
+                        $('#e').triggerHandler('click',function() {
+                            $('#info-bar').trigger('click');
+                        });
+                    });
+                });
+            });
+        });
+
+
+    });
+
+}
